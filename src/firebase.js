73002dce31bbc0771cf384a65d8df85ae8411834
IburiH-spec <bri.bri.ibu@gmail.com>
2026-06-04 20:12:54 +0900

    feat: LP-32 Authentication設定
    - firebase.jsにgetAuthのインポートを追加
    - authインスタンスを初期化してnamed exportで公開

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDfQ6f1HUD47tg-g4bNI2CrOZueT7dAUwE",
@@ -11,5 +12,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+// Firebase Authentication インスタンス
+export const auth = getAuth(app);
 
 export default app;
